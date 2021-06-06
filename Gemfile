69c1f06f94bfb054a21a9b812190b27f47150933
kazuta.nishkawa <kazuta.0206@gmail.com>
2021-06-06 07:14:27 +0000

    japanes can be on display

--- Gemfile
+++ Gemfile
@@ -58,3 +58,6 @@ group :production do
 end
 
 gem 'pry-byebug', group: :development
+
+#バリデーションエラーの日本語化gem
+gem 'rails-i18n'
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,10 @@ module Repmax
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    
+    #エラーメッセージの日本語化
+    config.i18n.default_locale = :ja
+    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
   end
 end
